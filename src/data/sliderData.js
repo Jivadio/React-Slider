037082ce65401fe7bf55a74ed74d4dec583422c4
Malo <42148894+Jivadio@users.noreply.github.com>
2024-06-23 22:43:45 +0200

    Add Slider Data

---
+++ src/data/sliderData.js
@@ -0,0 +1,22 @@
+export const sliderData = [
+    {
+        id: 1,
+        title: 'Slide 1',
+    },
+    {
+        id: 2,
+        title: 'Slide 2',
+    },
+    {
+        id: 3,
+        title: 'Slide 3',
+    },
+    {
+        id: 4,
+        title: 'Slide 4',
+    },
+    {
+        id: 5,
+        title: 'Slide 5',
+    },
+]
